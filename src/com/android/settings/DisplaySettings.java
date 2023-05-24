3f61d3e993e1c661e71720ac5995c97b7c933168
the projects <126561359+theprojectsorchid@users.noreply.github.com>
2023-05-24 00:32:36 +0000

    Orchidos Ui Dispaly : Fill out diplay settings add icons and others
    Change-Id: Ibd597fbabf0fe624b40730d2168aa08b93babcad
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>
    
    Settings: OverlayCategoryPreferenceController: Sort overlays by name
    
    No, the user has no reason to view em by priority, thank you.
    
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>
    
    Update anatolia_settings_navigation.xml
    
    Update display_settings.xml
    
    Update anatolia_settings_statusbar.xml
    
    display screen res
    
    OrchidOs display settings Ui clean up
    
    clean up the pages
    disolve customation page bring back refresh brightness levels relegate to lower
    promote auto rotate and fix up and arrange the back ground pages for screen settings and fonts and brightess sliders
    sort out a few lotties and sort the strings
    
    OrchidOs UI Display settings : statusbar fonts and icons
    
    display settings
    OrchidOs UI Display settings : sort out the page add icon and fotnts also
    replace lottie daylight arrange my autobrightness page

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -29,6 +29,7 @@ import com.android.settings.display.ShowOperatorNamePreferenceController;
 import com.android.settings.display.TapToWakePreferenceController;
 import com.android.settings.display.ThemePreferenceController;
 import com.android.settings.display.VrDisplayPreferenceController;
+import com.android.settings.development.OverlayCategoryPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.core.lifecycle.Lifecycle;
